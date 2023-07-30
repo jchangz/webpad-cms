199a132010f0dc135ce57a3f48d47e4ae6376401
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-07-30 16:51:53 -0600

    remove frontend

--- frontend/src/routes/pageRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { homeView } from "../controller/homepageController.ts"
-
-const pageRouter = express.Router()
-
-pageRouter.get("/", homeView)
-
-export default pageRouter
