4f2e99efed84f1db1bc24acbd4e8caf1b461b434
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-08-01 00:13:39 -0600

    build: setup docker dev environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd()),
+				// your custom rules
+				'/opt/app/'
+			]
+		}
+	},
+	cacheDir: 'cacheDir/node_modules/.vite',
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
